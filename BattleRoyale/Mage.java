db3325935be132f96712abab0a87c0c1430d9e49
wyliebenjamin2019 <42677949+wyliebenjamin2019@users.noreply.github.com>
2019-03-14 09:49:49 -0700

    Create Mage.java

---
+++ BattleRoyale/Mage.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+
+public class Mage extends PlayerCharacter{
+    String name;
+
+    private int wisdom;
+
+    public Mage(String newName) {
+        name = newName;
+
+        Random rand = new Random();
+
+        strength = 7 + (rand.nextInt(6) + 1);
+        intelligence = 15 + (rand.nextInt(6) + 1);
+        agility = 8 + (rand.nextInt(6) + 1);
+        wisdom = 10 + (rand.nextInt(6) + 1);
+
+        hp = hpmax = (strength);
+        mp = mpmax = (intelligence + 2 * wisdom);
+    }
+
+    public void showStats() {
+        System.out.printf("%s, a Mage:\n", name);
+        System.out.printf("WIS: %3d \n", wisdom);
+        super.showStats();
+    }
+}
