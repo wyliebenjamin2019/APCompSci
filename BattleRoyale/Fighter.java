07f2d2fe17226e03e69f687d2d0965de9177c1f1
wyliebenjamin2019 <42677949+wyliebenjamin2019@users.noreply.github.com>
2019-03-14 09:46:45 -0700

    Create Fighter.java

---
+++ BattleRoyale/Fighter.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+
+public class Fighter extends PlayerCharacter {
+
+    String name;
+
+    private int constitution;
+
+    public Fighter(String newName){
+
+        name = newName;
+
+        Random rand = new Random();
+
+        strength = 15 + (rand.nextInt(6)+1);
+        intelligence = 7 + (rand.nextInt(6)+1);
+        agility = 8 + (rand.nextInt(6)+1);
+        constitution = 10 + (rand.nextInt(6)+1);
+
+        hp = hpmax = (strength*2)+(2*constitution);
+        mp = mpmax = 0;
+    }
+
+    public void showStats() {
+        System.out.printf("%s, a Fighter:\n", name);
+        System.out.printf("CON: %3d \n", constitution);
+        super.showStats();
+    }
+}
