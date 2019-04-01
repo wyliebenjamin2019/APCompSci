1c1587f6adc56ccc5e9250d6acb4eaf6ad9a2f27
wyliebenjamin2019 <42677949+wyliebenjamin2019@users.noreply.github.com>
2019-04-01 08:30:25 -0700

    Create Escort.java

---
+++ Starship'd/Escort.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class Escort extends Starship implements IsDefense{
+
+
+    public Escort(String Name){
+
+        Random rand = new Random();
+        name = Name;
+
+        hull = 1000 + (rand.nextInt(250)+1);
+        shield = 2500 + (rand.nextInt(1000)+1);
+        beam = 2500 + (rand.nextInt(500)+1);
+        torpedo = 10 + (rand.nextInt(10)+1);
+
+        System.out.printf("%s:      Hull: %d    Shields: %d    Beam Weapons: %d    Torpedoes: %d", Name,hull,shield,beam,torpedo);
+        System.out.println();
+
+    }
+}
